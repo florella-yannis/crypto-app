cd3b73f8fa0f21292651976c2fa58f3ca71182bd
florella-yannis <florellayannis@gmail.com>
2023-11-07 20:45:17 +0100

    api connect

---
+++ src/components/GlobalChart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GlobalChart = ({ coinsData }) => {
+    console.log(coinsData);
+
+    return (
+        <div className="global-chart">
+            <h1>test</h1>
+        </div>
+    );
+};
+
+export default GlobalChart;
