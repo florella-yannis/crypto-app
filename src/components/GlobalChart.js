fe7c189f395c5fca67cd038a1dbecf0e7389061d
florella-yannis <florellayannis@gmail.com>
2023-11-07 21:20:46 +0100

    rechart treemap + npm
    https://recharts.org/en-US

--- src/components/GlobalChart.js
+++ src/components/GlobalChart.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Treemap } from 'recharts';
 
 const GlobalChart = ({ coinsData }) => {
-    console.log(coinsData);
+    // console.log(coinsData);
+
+    const [dataArray, setDataArray] = useState([]);
+
+    useEffect(() => {
+        let chartData = [];
+
+        if(coinsData.length > 0) {
+            for(let i = 0; i < 45; i++) {
+                chartData.push({
+                    name: coinsData[i].symbol.toUpperCase() + " " + coinsData[i].market_cap_change_percentage_24h.toFixed(1) + "%",
+                    size: coinsData[i].market_cap,
+                    fill: null,
+
+                })
+            }
+        }
+        // console.log(chartData);
+        setDataArray(chartData);
+    },[coinsData])
 
     return (
         <div className="global-chart">
-            <h1>test</h1>
+            <Treemap
+            width={730}
+            height={181}
+            data={dataArray}
+            dataKey="size"
+            stroke="rgb(51,51,51)"
+            fill="black"
+            aspectRatio="1"></Treemap>
         </div>
     );
 };
