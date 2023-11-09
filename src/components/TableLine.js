ba5ad7db2024fa31071c42f697539e12241ece40
florella-yannis <florellayannis@gmail.com>
2023-11-09 22:45:27 +0100

    Update TableLine.js

--- src/components/TableLine.js
+++ src/components/TableLine.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PercentChange from './PercentChange';
 
 const TableLine = ({ coin, index }) => {
 
@@ -38,6 +39,18 @@ const TableLine = ({ coin, index }) => {
             </div>
             <p>{priceFormater(coin.current_price).toLocaleString() + " $"}</p>
             <p className="mktcap">{mktCapFormater(coin.market_cap).toLocaleString() + "M$"}</p>
+            <p className="volume">{coin.total_volume.toLocaleString()} $</p>
+            <PercentChange percent={coin.price_change_percentage_1h_in_currency} />
+            <PercentChange percent={coin.market_cap_change_percentage_24h} />
+            <PercentChange percent={coin.price_change_percentage_7d_in_currency} />
+            <PercentChange percent={coin.price_change_percentage_30d_in_currency} />
+            <PercentChange percent={coin.price_change_percentage_200d_in_currency} />
+            <PercentChange percent={coin.price_change_percentage_1y_in_currency} />
+            {coin.ath_change_percentage > -3 ? (
+                <p>ATH !</p>
+            ) : (
+            <PercentChange percent={coin.ath_change_percentage} />
+            )}
         </div>
     );
 };
