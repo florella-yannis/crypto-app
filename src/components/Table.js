88e183d1a2e3428b241e0bc1faa9f85311884486
florella-yannis <florellayannis@gmail.com>
2023-11-13 19:19:01 +0100

    sort with switch

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,9 +5,20 @@ import ToTop from "./ToTop";
 const Table = ({ coinsData }) => {
     // console.log(coinsData);
     const [rangeNumber, setRangeNumber] = useState(100);
-    const[orderBy, setOrderBy] = useState("");
+    const [orderBy, setOrderBy] = useState("");
 
-    const tableHeader = ["Prix", "MarketCap", "Volume", "1h", "1j", "1s", "1m", "6m", "1a", "ATH"];
+    const tableHeader = [
+        "Prix",
+        "MarketCap",
+        "Volume",
+        "1h",
+        "1j",
+        "1s",
+        "1m",
+        "6m",
+        "1a",
+        "ATH",
+    ];
 
     return (
         <div className="table-container">
@@ -32,27 +43,113 @@ const Table = ({ coinsData }) => {
                 </div>
                 {tableHeader.map((el) => (
                     <li key={el}>
-                        <input type="radio" name="header-el" id={el} defaultChecked=
-                        {
-                            el === orderBy || el === orderBy + "reverse" ? true : false
-                        } 
-                        onClick={() => {
-                            if (orderBy === el) {
-                                setOrderBy(el + "reverse");
-                            } else {
-                                setOrderBy(el);
+                        <input
+                            type="radio"
+                            name="header-el"
+                            id={el}
+                            defaultChecked={
+                                el === orderBy || el === orderBy + "reverse" ? true : false
                             }
-                        }}
+                            onClick={() => {
+                                if (orderBy === el) {
+                                    setOrderBy(el + "reverse");
+                                } else {
+                                    setOrderBy(el);
+                                }
+                            }}
                         />
                         <label htmlFor={el}>{el}</label>
                     </li>
                 ))}
             </ul>
-            {coinsData && coinsData
-            .slice(0, rangeNumber)
-            .map((coin, index) => (
-                <TableLine coin={coin} index={index} />
-            ))}
+            {coinsData &&
+                coinsData
+                    .slice(0, rangeNumber)
+                    .sort((a, b) => {
+                        switch (orderBy) {
+                            case "Prix":
+                                return b.current_price - a.current_price;
+                            case "Volume":
+                                return b.total_volume - a.total_volume;
+                            case "MarketCap":
+                                return b.market_cap - a.market_cap;
+                            case "1h":
+                                return (
+                                    b.price_change_percentage_1h_in_currency -
+                                    a.price_change_percentage_1h_in_currency
+                                );
+                            case "1j":
+                                return (
+                                    b.market_cap_change_percentage_24h -
+                                    a.market_cap_change_percentage_24h
+                                );
+                            case "1s":
+                                return (
+                                    b.price_change_percentage_7d_in_currency -
+                                    a.price_change_percentage_7d_in_currency
+                                );
+                            case "1m":
+                                return (
+                                    b.price_change_percentage_30d_in_currency -
+                                    a.price_change_percentage_30d_in_currency
+                                );
+                            case "6m":
+                                return (
+                                    b.price_change_percentage_200d_in_currency -
+                                    a.price_change_percentage_200d_in_currency
+                                );
+                            case "1a":
+                                return (
+                                    b.price_change_percentage_1y_in_currency -
+                                    a.price_change_percentage_1y_in_currency
+                                );
+                            case "ATH":
+                                return b.ath_change_percentage - a.ath_change_percentage;
+                            case "#reverse":
+                                return a.market_cap - b.market_cap;
+                            case "Prixreverse":
+                                return a.current_price - b.current_price;
+                            case "Volumereverse":
+                                return a.total_volume - b.total_volume;
+                            case "MarketCapreverse":
+                                return a.market_cap - b.market_cap;
+                            case "1hreverse":
+                                return (
+                                    a.price_change_percentage_1h_in_currency -
+                                    b.price_change_percentage_1h_in_currency
+                                );
+                            case "1jreverse":
+                                return (
+                                    a.market_cap_change_percentage_24h -
+                                    b.market_cap_change_percentage_24h
+                                );
+                            case "1sreverse":
+                                return (
+                                    a.price_change_percentage_7d_in_currency -
+                                    b.price_change_percentage_7d_in_currency
+                                );
+                            case "1mreverse":
+                                return (
+                                    a.price_change_percentage_30d_in_currency -
+                                    b.price_change_percentage_30d_in_currency
+                                );
+                            case "6mreverse":
+                                return (
+                                    a.price_change_percentage_200d_in_currency -
+                                    b.price_change_percentage_200d_in_currency
+                                );
+                            case "1areverse":
+                                return (
+                                    a.price_change_percentage_1y_in_currency -
+                                    b.price_change_percentage_1y_in_currency
+                                );
+                            case "ATHreverse":
+                                return a.ath_change_percentage - b.ath_change_percentage;
+                            default:
+                                return null;
+                    }
+                    })
+                    .map((coin, index) => <TableLine coin={coin} index={index} />)}
         </div>
     );
 };
